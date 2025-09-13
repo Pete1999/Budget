e20bb661fc7458972f5a79aaf7b6a604cd44e812
Pete1999 <peter.shand@sympatico.ca>
2025-09-13 17:54:16 -0400

    Start with creating DataTableModel from demo data. Demo most basic MainWindow from test with test data from file.
    Comlete. Add column type. Probably need Abstract.
    Extending DefaultTableModel to override getColumClass--tested

--- src/test/java/budget/ui/BudgetCategoryDataTest.java
+++ src/test/java/budget/ui/BudgetCategoryDataTest.java
@@ -1,10 +1,11 @@
 package budget.ui;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.table.TableModel;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class BudgetCategoryDataTest {
     BudgetCategoryData subject;
@@ -16,8 +17,15 @@ class BudgetCategoryDataTest {
 
     @Test
     void testGetTableModel() {
-        TableModel tm = subject.getTableModel();
-        Assertions.assertTrue(true);
+        TableModel tm = BudgetCategoryData.getTableModel();
+        assertInstanceOf(TableModel.class, tm);
 
+    }
+
+    @Test
+    void getColumnClassTest() {
+
+        assertEquals(Integer.class, subject.getColumnClass(3));
+        assertEquals(String.class, subject.getColumnClass(0));
     }
 }
